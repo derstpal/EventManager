7a5f326492855909e0b811cfc2cc97546ddc7c93
MS <mathieu.secreitn@gmail.com>
2022-05-18 17:37:04 +0200

    Add connectioncomp

--- EventComponentWeb/src/app/components/connect/connect.component.ts
+++ EventComponentWeb/src/app/components/connect/connect.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './../../services/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-connect',
   templateUrl: './connect.component.html',
-  styleUrls: ['./connect.component.scss']
+  styleUrls: ['./connect.component.scss'],
 })
-export class ConnectComponent implements OnInit {
+export class ConnectComponent implements OnInit, OnDestroy {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  isAuthSubscription: Subscription | undefined;
+  public isAuth: Boolean | null = false;
 
   ngOnInit(): void {
+    this.isAuthSubscription = this.authService.isAuthSubject.subscribe(
+      (value) => (this.isAuth = value)
+    );
   }
 
+  ngOnDestroy(): void {
+    this.isAuthSubscription?.unsubscribe();
+  }
+
+  onConnect(): void {
+    this.isAuth = null;
+    this.authService.ConnectAsync().then(
+      () => {
+        console.log('connected');
+      },
+      (e) => {
+        console.error(e);
+      }
+    );
+  }
 }
--- EventComponentWeb/src/app/services/auth.service.ts
+++ EventComponentWeb/src/app/services/auth.service.ts
@@ -1,21 +1,30 @@
-import { Subject } from "rxjs/internal/Subject";
+import { Subject } from 'rxjs/internal/Subject';
 
 export class AuthService {
-  public isAuth = false;
+  public isAuth: Boolean | null = false;
 
-  public isAuthSubject: Subject<Boolean> = new Subject<Boolean>();
+  public isAuthSubject: Subject<Boolean | null> = new Subject<Boolean | null>();
 
-  public Connect(): void {
-    this.isAuth = true;
-    this.emitIsAuthSubject();
+  public ConnectAsync() {
+    this.SetIsAuthSubject(null);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(true);
+        this.SetIsAuthSubject(true);
+      }, 1000);
+    });
   }
 
   public Disonnect(): void {
-    this.isAuth = false;
-    this.emitIsAuthSubject();
+    this.SetIsAuthSubject(false);
   }
 
   public emitIsAuthSubject() {
     this.isAuthSubject.next(this.isAuth);
+  }
+
+  private SetIsAuthSubject(value: Boolean | null): void {
+    this.isAuth = value;
+    this.emitIsAuthSubject();
   }
 }
