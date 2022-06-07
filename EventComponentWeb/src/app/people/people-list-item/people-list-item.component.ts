0ca5eef0c48ef1d72d9f43d6b69f8d97fe1ac60d
MS <mathieu.secreitn@gmail.com>
2022-06-07 11:22:56 +0200

    list item people

--- EventComponentWeb/src/app/people/people-list-item/people-list-item.component.ts
+++ EventComponentWeb/src/app/people/people-list-item/people-list-item.component.ts
@@ -1,13 +1,14 @@
 import { peopleEntity } from './../models/peopleEntity';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-people-list-item',
   templateUrl: './people-list-item.component.html',
-  styleUrls: ['./people-list-item.component.scss']
+  styleUrls: ['./people-list-item.component.scss'],
 })
-export class PeopleListItemComponent{
+export class PeopleListItemComponent {
   @Input() people: peopleEntity | undefined;
-
-  constructor() { }
+  today: moment.Moment = moment(new Date());
+  constructor() {}
 }
