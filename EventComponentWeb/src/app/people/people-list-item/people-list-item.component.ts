86af53f113e6e44bdf14dd618dd30de55e9f083b
MS <mathieu.secreitn@gmail.com>
2022-06-08 13:59:34 +0200

    ngmodel

--- EventComponentWeb/src/app/people/people-list-item/people-list-item.component.ts
+++ EventComponentWeb/src/app/people/people-list-item/people-list-item.component.ts
@@ -15,24 +15,13 @@ import * as moment from 'moment';
   styleUrls: ['./people-list-item.component.scss'],
 })
 export class PeopleListItemComponent {
-  @Input() people: peopleEntity | undefined;
+  @Input()
+  people!: peopleEntity;
   @Input() hasSelection: Boolean | undefined;
-
-  @Output() isSelectedChange = new EventEmitter<boolean>();
-  @Input() isSelected: Boolean | undefined;
 
   background: string = 'transparent';
 
   today: moment.Moment = moment(new Date());
-  constructor() {}
-
-  onSelectChange(value: boolean) {
-    this.isSelected = value;
-    this.background = value ? '#0d6efd' : 'transparent';
-    console.log(`Selection ${this.isSelected} on ${this.people?.key}`);
-    this.isSelectedChange.emit(value);
-    if (this.people) {
-      this.people.isSelected = value;
-    }
+  constructor() {
   }
 }
